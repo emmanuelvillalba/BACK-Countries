7c4e438a835a31f8587015fcbeea4a0f0229f971
EmaVillalba <emasv23@gmail.com>
2024-04-21 20:27:40 -0300

    TESTEANDO VARIABLE DE ENTORNO PORT_SERVER

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const server = require("./src/server");
 const { conn } = require("./src/db.js");
-const {PORT} = process.env || 3001;
+const {PORT_SERVER} = process.env || 3001;
 const upDateDB = require("./src/utils/upDateDB.js");
 
 async function startServer() {
